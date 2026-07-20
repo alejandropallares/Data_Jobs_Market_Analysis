3a33a0b5391d7d656466ea2ad7c315033eefda32
Alejandro Pallares <alejandropallares10@gmail.com>
2026-07-19 22:50:41 -0600

    Add new query to answer thw top companies that have the best avg salary by rol

--- Business_Questions/01_salary_analysis.sql
+++ Business_Questions/01_salary_analysis.sql
@@ -76,3 +76,145 @@ FROM (
     GROUP BY job_title_short
 ) 
 ORDER BY salary_difference;
+
+
+-- Business Question 3: Which country has the highest salary average for each rol? 
+
+
+SELECT 
+    salary_by_country.job_title_short,
+    salary_by_country.job_country,
+    salary_by_country.salary_year_avg_by_position AS top_avg_salary
+
+FROM (
+    SELECT 
+        job_title_short,
+        job_country,
+        AVG(salary_year_avg) AS salary_year_avg_by_position
+
+    FROM job_postings_fact
+
+    WHERE 
+        job_country IS NOT NULL
+        AND salary_year_avg IS NOT NULL
+
+    GROUP BY 
+        job_title_short,
+        job_country
+) AS salary_by_country
+
+INNER JOIN (
+    SELECT 
+        job_title_short,
+        MAX(salary_year_avg_by_position) AS top_salary
+
+    FROM (
+        SELECT 
+            job_title_short,
+            job_country,
+            AVG(salary_year_avg) AS salary_year_avg_by_position
+
+        FROM job_postings_fact
+
+        WHERE 
+            job_country IS NOT NULL
+            AND salary_year_avg IS NOT NULL
+
+        GROUP BY 
+            job_title_short,
+            job_country
+    ) AS salary_by_country
+
+    GROUP BY 
+        job_title_short
+) AS top_salary_by_role
+
+    ON salary_by_country.job_title_short = top_salary_by_role.job_title_short
+    AND salary_by_country.salary_year_avg_by_position = top_salary_by_role.top_salary
+
+ORDER BY 
+    top_avg_salary DESC;
+
+
+
+/*───────────────────────────┬───────────────────────────────────┬────────────────────┐
+│      job_title_short      │            job_country            │   top_avg_salary   │
+│          varchar          │              varchar              │       double       │
+├───────────────────────────┼───────────────────────────────────┼────────────────────┤
+│ Data Analyst              │ Belarus                           │           400000.0 │
+│ Data Engineer             │ Panama                            │  383573.6842105263 │
+│ Senior Data Scientist     │ South Africa                      │         328709.125 │
+│ Data Scientist            │ Russia                            │           285000.0 │
+│ Cloud Engineer            │ Russia                            │           280000.0 │
+│ Machine Learning Engineer │ Northern Mariana Islands          │           275000.0 │
+│ Machine Learning Engineer │ Cayman Islands                    │           275000.0 │
+│ Software Engineer         │ Congo, Democratic Republic of the │           275000.0 │
+│ Business Analyst          │ Curaçao                           │           264000.0 │
+│ Business Analyst          │ Guyana                            │           264000.0 │
+│ Senior Data Engineer      │ Czechia                           │ 239433.33333333334 │
+│ Senior Data Analyst       │ Panama                            │           160000.0 │
+└───────────────────────────┴───────────────────────────────────┴────────────────────┘*/
+
+
+-- Busines Question 4 ¿Qué empresas ofrecen los salarios promedio más altos para cada rol?
+
+SELECT 
+    salary_by_company.job_title_short,
+    salary_by_company.company_name,
+    salary_by_company.avg_salary AS highest_average_salary
+
+FROM (
+    SELECT 
+        jpf.job_title_short,
+        cd.name AS company_name,
+        AVG(jpf.salary_year_avg) AS avg_salary
+
+    FROM job_postings_fact AS jpf
+
+    INNER JOIN company_dim AS cd
+        ON jpf.company_id = cd.company_id
+
+    WHERE 
+        jpf.salary_year_avg IS NOT NULL
+
+    GROUP BY 
+        jpf.job_title_short,
+        cd.name
+) AS salary_by_company
+
+INNER JOIN (
+    SELECT 
+        job_title_short,
+        MAX(avg_salary) AS highest_average_salary
+
+    FROM (
+        SELECT 
+            jpf.job_title_short,
+            cd.name AS company_name,
+            AVG(jpf.salary_year_avg) AS avg_salary
+
+        FROM job_postings_fact AS jpf
+
+        INNER JOIN company_dim AS cd
+            ON jpf.company_id = cd.company_id
+
+        WHERE 
+            jpf.salary_year_avg IS NOT NULL
+
+        GROUP BY 
+            jpf.job_title_short,
+            cd.name
+    ) AS salary_by_company
+
+    GROUP BY 
+        job_title_short
+) AS top_avg_salary_by_role
+
+    ON salary_by_company.job_title_short =
+       top_avg_salary_by_role.job_title_short
+
+    AND salary_by_company.avg_salary =
+        top_avg_salary_by_role.highest_average_salary
+
+ORDER BY 
+    highest_average_salary DESC;
